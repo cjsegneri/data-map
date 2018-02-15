67001d005e94cfedb2ec27d3be0d801b53e13734
Connor Jason Segneri <cjsegneri@cox.net>
2018-02-14 17:29:16 -0700

    added clusters and revamped ui

--- global.R
+++ global.R
@@ -1,5 +1,7 @@
 # libraries
 library(shinydashboard)
+library(shinyWidgets)
+library(shinythemes)
 library(leaflet)
 library(zipcode)
 
@@ -7,24 +9,24 @@ library(zipcode)
 data(zipcode)
 
 # get ncmec data
-attempts = read.csv("ncmecData/Attempts_Hackathon_5_Years_of_Data.csv")
+kidnapping = read.csv("ncmecData/Attempts_Hackathon_5_Years_of_Data.csv")
 missing = read.csv("ncmecData/Hackathon_Missing_Child_5_Years_of_Data.csv")
 
 # change the zipcode column to "zip" to match zipcode data
-colnames(attempts)[which(names(attempts) == "Incident.Zip")] <- "zip"
-colnames(missing)[which(names(missing) == "Missing.Zip")] <- "zip"
+colnames(kidnapping)[which(names(kidnapping) == "Incident.Zip")] = "zip"
+colnames(missing)[which(names(missing) == "Missing.Zip")] = "zip"
 
 # merge lat/lon values into datasets by zip code
-attempts = merge(attempts, zipcode, by = "zip")
+kidnapping = merge(kidnapping, zipcode, by = "zip")
 missing = merge(missing, zipcode, by = "zip")
 
 # fix the dates
-attempts$Incident.Date = as.Date(attempts$Incident.Date, format = "%d/%m/%Y")
+kidnapping$Incident.Date = as.Date(kidnapping$Incident.Date, format = "%d/%m/%Y")
 missing$Missing.Date = as.Date(missing$Missing.Date, format = "%d/%m/%Y")
 
 # fix the -1 values in the method columns
-attempts$Offender.Method.Animal[attempts$Offender.Method.Animal==-1] = 1
-attempts$Offender.Method.Candy[attempts$Offender.Method.Candy==-1] = 1
-attempts$Offender.Method.Money[attempts$Offender.Method.Money==-1] = 1
-attempts$Offender.Method.Other[attempts$Offender.Method.Other==-1] = 1
-attempts$Offender.Method.Ride[attempts$Offender.Method.Ride==-1] = 1
+kidnapping$Offender.Method.Animal[kidnapping$Offender.Method.Animal==-1] = 1
+kidnapping$Offender.Method.Candy[kidnapping$Offender.Method.Candy==-1] = 1
+kidnapping$Offender.Method.Money[kidnapping$Offender.Method.Money==-1] = 1
+kidnapping$Offender.Method.Other[kidnapping$Offender.Method.Other==-1] = 1
+kidnapping$Offender.Method.Ride[kidnapping$Offender.Method.Ride==-1] = 1
--- server.R
+++ server.R
@@ -1,4 +1,129 @@
 
 function(input, output, session) {
 
+  whichSets = reactive({
+    if (input$missing_switch & input$kidnapping_switch) { return (1) }
+    if (input$missing_switch & !input$kidnapping_switch) { return (2) }
+    if (!input$missing_switch & input$kidnapping_switch) { return (3) }
+    if (!input$missing_switch & !input$kidnapping_switch) { return (4) }
+  })
+
+  getMissing = reactive({
+    filtered = missing
+
+    # check for state
+    if (!is.null(input$m_state_inp)) { filtered = filtered[filtered$state %in% input$m_state_inp,] }
+    # check for city
+    if (!is.null(input$m_city_inp)) { filtered = filtered[filtered$city %in% input$m_city_inp,] }
+    # check for zip
+    if (!is.null(input$m_zip_inp)) { filtered = filtered[filtered$zip %in% input$m_zip_inp,] }
+
+    return (filtered)
+  })
+
+  getKidnapping = reactive({
+    filtered = kidnapping
+
+    # check for state
+    if (!is.null(input$k_state_inp)) { filtered = filtered[filtered$state %in% input$k_state_inp,] }
+    # check for city
+    if (!is.null(input$k_city_inp)) { filtered = filtered[filtered$city %in% input$k_city_inp,] }
+    # check for zip
+    if (!is.null(input$k_zip_inp)) { filtered = filtered[filtered$zip %in% input$k_zip_inp,] }
+
+    return (filtered)
+  })
+
+  output$map = renderLeaflet({
+
+    # read in the missing and kidnapping data
+    missing.filtered = getMissing()
+    kidnapping.filtered = getKidnapping()
+
+    # read in which datasets to show
+    sets = whichSets()
+
+    # create the map
+    if (sets == 1) {
+      leaflet() %>% addProviderTiles(providers$CartoDB.Positron) %>%
+        addCircleMarkers(
+          data = missing.filtered, lng = ~longitude, lat = ~latitude, color = "#ff6666",
+          clusterOptions = markerClusterOptions(iconCreateFunction =
+                                                  JS("
+                                                     function(cluster) {
+                                                     return new L.DivIcon({
+                                                     html: '<div style=\"background-color:rgba(255,102,102,0.8)\"><span>' + cluster.getChildCount() + '</div><span>',
+                                                     className: 'marker-cluster'
+                                                     });
+                                                     }")
+          )
+                                                  ) %>%
+        addCircleMarkers(
+          data = kidnapping, lng = ~longitude, lat = ~latitude, color = "#66b2ff",
+          clusterOptions = markerClusterOptions(iconCreateFunction =
+                                                  JS("
+                                                     function(cluster) {
+                                                     return new L.DivIcon({
+                                                     html: '<div style=\"background-color:rgba(102,178,255,0.8)\"><span>' + cluster.getChildCount() + '</div><span>',
+                                                     className: 'marker-cluster'
+                                                     });
+                                                     }")
+          )
+                                                  ) %>%
+        addLegend(
+          "bottomleft", title = "Datasets", labels = c("Missing Persons", "Attempted Kidnapping"),
+          colors = c("#ff6666","#66b2ff"), opacity = 0.5
+        )
+    } else if (sets == 2) {
+      leaflet() %>% addProviderTiles(providers$CartoDB.Positron) %>%
+        addCircleMarkers(
+          data = missing.filtered, lng = ~longitude, lat = ~latitude, color = "#ff6666",
+          clusterOptions = markerClusterOptions(iconCreateFunction =
+                                                  JS("
+                                                     function(cluster) {
+                                                     return new L.DivIcon({
+                                                     html: '<div style=\"background-color:rgba(255,102,102,0.8)\"><span>' + cluster.getChildCount() + '</div><span>',
+                                                     className: 'marker-cluster'
+                                                     });
+                                                     }")
+          )
+                                                  ) %>%
+        addLegend(
+          "bottomleft", title = "Datasets", labels = c("Missing Persons", "Attempted Kidnapping"),
+          colors = c("#ff6666","#66b2ff"), opacity = 0.5
+        )
+    } else if (sets == 3) {
+      leaflet() %>% addProviderTiles(providers$CartoDB.Positron) %>%
+        addCircleMarkers(
+          data = kidnapping, lng = ~longitude, lat = ~latitude, color = "#66b2ff",
+          clusterOptions = markerClusterOptions(iconCreateFunction =
+                                                  JS("
+                                                     function(cluster) {
+                                                     return new L.DivIcon({
+                                                     html: '<div style=\"background-color:rgba(102,178,255,0.8)\"><span>' + cluster.getChildCount() + '</div><span>',
+                                                     className: 'marker-cluster'
+                                                     });
+                                                     }")
+          )
+                                                  ) %>%
+        addLegend(
+          "bottomleft", title = "Datasets", labels = c("Missing Persons", "Attempted Kidnapping"),
+          colors = c("#ff6666","#66b2ff"), opacity = 0.5
+        )
+    } else {
+      leaflet() %>% addProviderTiles(providers$CartoDB.Positron)
+    }
+  })
+
+
+  output$download_btn = downloadHandler(
+    filename = function() {
+      paste("data-", Sys.Date(), ".csv", sep="")
+    },
+    content = function(file) {
+      missing.filtered = getMissing()
+      write.csv(missing.filtered, file)
+    }
+  )
+
 }
